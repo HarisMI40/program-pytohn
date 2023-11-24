0dcdc9fc926783f936bc49b76a90f076566c2552
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2023-11-24 15:54:01 +0700

    Add files via upload

---
+++ latiham_fungsi.py
@@ -0,0 +1,8 @@
+def hitung(angka1, angka2, operator) :
+    #    kalau operator = *
+        # angka1 * angka2
+    if operator == "*" :
+        return angka1 * angka2
+
+print(hitung(2,4, "*"))
+print(hitung(2,4, "+"))
